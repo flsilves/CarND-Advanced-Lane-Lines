39ff6410ac9a01cf1aa9b10ea26b0bdce5f5505c
Flavio Silvestre <flaviosilvestre89@gmail.com>
2020-10-20 01:03:23 +0200

    WIP

--- tests/test_threshold.py
+++ tests/test_threshold.py
@@ -16,13 +16,13 @@ from test_utils import *
 TEST_OUTPUT_DIR = 'test_threshold_images'
 
 
-class ThresholdImageTest(unittest.TestCase):
+class CameraCalibrationTest(unittest.TestCase):
     def setUp(self):
         logging.basicConfig(level=logging.INFO, format='%(message)s')
-        road_images = glob.glob(
-            f'{ROAD_IMAGES_DIR}/*.jpg')
+        calibration_images = glob.glob(
+            f'{CALIBRATION_IMAGES_DIR}/calibration*.jpg')
         self.camera = Camera(nx=9, ny=6, calibration_images=calibration_images,
-                             calibration_filename='../calibration.pickle')
+                             calibration_filename=CALIBRATION_FILE)
 
     def tearDown(self):
         return
@@ -32,13 +32,13 @@ class ThresholdImageTest(unittest.TestCase):
         logging.info('Applying threshold on road images')
 
         for idx, test_image in enumerate(test_images):
-            logging.debug("Image %d", idx)
+            logging.debug('Image %d', idx)
 
             undistorted_image = self.camera.undistort_image(test_image)
 
             edge_detector = EdgeDetector()
             edges = edge_detector.detect(undistorted_image)
-            filename = f"{TEST_OUTPUT_DIR}/threshold_{str(idx)}_undistorted.png"
+            filename = f'{TEST_OUTPUT_DIR}/threshold_{str(idx)}_undistorted.png'
             save_before_and_after_image(
                 test_image, edges, filename)
 
@@ -50,7 +50,7 @@ if __name__ == '__main__':
         os.makedirs(TEST_OUTPUT_DIR)
     else:
         files = glob.glob(f'{TEST_OUTPUT_DIR}/*.png')
-        logging.info("Deleting %d images from previous run", len(files))
+        logging.info('Deleting %d images from previous run', len(files))
         for f in files:
             os.remove(f)
 
