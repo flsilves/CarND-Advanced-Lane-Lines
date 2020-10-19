624a69fae303d1cbd164eb575d01024861a74a2b
Flavio Silvestre <flaviosilvestre89@gmail.com>
2020-10-20 00:54:14 +0200

    WIP

--- tests/test_camera_calibration.py
+++ tests/test_camera_calibration.py
@@ -22,53 +22,11 @@ ROAD_IMAGES_DIR = "../test_images/"
 TEST_OUTPUT_DIR = "test_camera_calibration_output"
 
 
-def get_images_from_dir(path):
-    image_list = []
-    for filename in glob.glob(f'{path}/*.jpg'):
-        jpg_image = cv2.imread(filename)
-        jpg_image = cv2.cvtColor(jpg_image, cv2.COLOR_BGR2RGB)
-        image_list.append(jpg_image)
-
-    for filename in glob.glob(f'{path}/*.png'):
-        png_image = cv2.imread(filename)
-        png_image = cv2.cvtColor(png_image, cv2.COLOR_BGR2RGB)
-        image_list.append(png_image)
-
-    return image_list
-
-
-def save_before_and_after_image(before_img, after_img, save_file):
-
-    image_dpi = 72
-
-    width_inches = int(2*before_img.shape[1]) / image_dpi
-    height_inches = int(2*before_img.shape[0]) / image_dpi
-
-    logging.debug("width %f height:%f", width_inches, height_inches)
-    figsize = (width_inches, height_inches)
-
-    figure, (ax1, ax2) = plt.subplots(
-        1, 2, figsize=figsize, dpi=image_dpi, frameon=False)
-
-    figure.tight_layout()
-    ax1.imshow(before_img)
-    ax1.axis('off')
-    ax1.set_title("original", fontsize=25)
-    ax2.axis('off')
-
-    ax2.imshow(after_img)
-    ax2.set_title("undistorted", fontsize=25)
-
-    figure.savefig(save_file, dpi='figure',
-                   bbox_inches='tight')
-
-    plt.close('all')
-
-
 class CameraCalibrationTest(unittest.TestCase):
     def setUp(self):
         logging.basicConfig(level=logging.INFO, format='%(message)s')
-        calibration_images = glob.glob('../camera_cal/calibration*.jpg')
+        calibration_images = glob.glob(
+            f'{CALIBRATION_IMAGES_DIR}/calibration*.jpg')
         self.camera = Camera(nx=9, ny=6, calibration_images=calibration_images,
                              calibration_filename='../calibration.pickle')
 
--- tests/test_threshold.py
+++ tests/test_threshold.py
@@ -17,7 +17,6 @@ sys.path.append("..")  # nopep8
 from camera import Camera  # nopep8
 from threshold import *  # nopep8
 
-CALIBRATION_IMAGES_DIR = "../camera_cal/"
 ROAD_IMAGES_DIR = "../test_images/"
 
 TEST_OUTPUT_DIR = "test_threshold_images"
@@ -26,12 +25,28 @@ TEST_OUTPUT_DIR = "test_threshold_images"
 class ThresholdImageTest(unittest.TestCase):
     def setUp(self):
         logging.basicConfig(level=logging.INFO, format='%(message)s')
-        calibration_images = glob.glob('../camera_cal/calibration*.jpg')
+        road_images = glob.glob(
+            f'{ROAD_IMAGES_DIR}/*.jpg')
         self.camera = Camera(nx=9, ny=6, calibration_images=calibration_images,
                              calibration_filename='../calibration.pickle')
 
     def tearDown(self):
         return
+
+    def xtest_threshold_road_images(self):
+        test_images = get_images_from_dir(ROAD_IMAGES_DIR)
+        logging.info("Applying threshold on road images")
+
+        for idx, test_image in enumerate(test_images):
+            logging.debug("Image %d", idx)
+
+            undistorted_image = self.camera.undistort_image(test_image)
+
+            edge_detector = EdgeDetector()
+            edges = edge_detector.detect(undistorted_image)
+            filename = f"{TEST_OUTPUT_DIR}/threshold_{str(idx)}_undistorted.png"
+            save_before_and_after_image(
+                test_image, edges, filename)
 
 
 if __name__ == '__main__':
